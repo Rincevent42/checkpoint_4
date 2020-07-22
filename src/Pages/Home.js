a450863fecce02333c88fea417edd39e40e9c16d
Christophe <porvooc@gmail.com>
2020-07-22 16:03:32 +0200

    Avant mise en place carousel

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../styles/Home.css';
+import Carousel from '../components/Carousel';
+const cars = require('../voitures.json');
+
 
 export default function Home (props) {
+
   return (
     <div className='home'>
       <div className='home-text'>
@@ -9,6 +13,11 @@ export default function Home (props) {
       </div>
       <div className='novelties'>
         <p>Nouveautés</p>
+        <div className='slider'>
+          <div className='slider-images'>
+            <Carousel cars={cars.data} />
+          </div>
+        </div>
       </div>
     </div>
   );
